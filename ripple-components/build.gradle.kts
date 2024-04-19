0e276fac2d6cb446d17fe14818941c1bdebfe39c
ConcerroX <concerrox@outlook.com>
2024-04-19 22:07:27 +0800

    Add Maven configuration.

--- ripple-components/build.gradle.kts
+++ ripple-components/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
-    id("signing")
+//    id("signing")
 }
 
 android {
@@ -31,58 +31,49 @@ android {
 }
 
 publishing {
-    repositories {
-        mavenCentral {
-            credentials {
-
-            }
-        }
-    }
     publications {
         register<MavenPublication>("main") {
-            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
             groupId = "io.github.concerrox"
             artifactId = "ripple"
             version = "1.0.0-SNAPSHOT"
-            pom {
-                name = "Ripple Components for Android"
-                description = ""
-                url = "https://github.com/ConcerroX/ripple-components-android"
-                inceptionYear = "2024"
-//                artifact("$buildDir/outputs/aar/ripple-components-release.aar")
-                scm {
-                    url = "https://github.com/ConcerroX/ripple-components-android"
-                    connection =
-                        "scm:git@https://github.com/ConcerroX/ripple-components-android.git"
-                    developerConnection = "scm:git@https://github.com/ConcerroX/ripple-components-android.git"
-                }
-                licenses {
-                    license {
-                        name = "The Apache Software License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                        distribution = "repo"
-                        comments = "A business-friendly OSS license"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "concerrox"
-                        name = "ConcerroX"
-                        email = "concerrox@outlook.com"
-                        url = "https://github.com/ConcerroX"
-                    }
-                }
-                issueManagement {
-                    system = "Github"
-                    url = "https://github.com/ConcerroX/ripple-components-android/issues"
-                }
+            afterEvaluate {
+                artifact(tasks.getByName("bundleReleaseAar"))
             }
+//            pom {
+//                name = "Ripple Components for Android"
+//                description = ""
+//                url = "https://github.com/ConcerroX/ripple-components-android"
+//                inceptionYear = "2024"
+////                artifact("$buildDir/outputs/aar/ripple-components-release.aar")
+//                scm {
+//                    url = "https://github.com/ConcerroX/ripple-components-android"
+//                    connection =
+//                        "scm:git@https://github.com/ConcerroX/ripple-components-android.git"
+//                    developerConnection = "scm:git@https://github.com/ConcerroX/ripple-components-android.git"
+//                }
+//                licenses {
+//                    license {
+//                        name = "The Apache Software License, Version 2.0"
+//                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+//                        distribution = "repo"
+//                        comments = "A business-friendly OSS license"
+//                    }
+//                }
+//                developers {
+//                    developer {
+//                        id = "concerrox"
+//                        name = "ConcerroX"
+//                        email = "concerrox@outlook.com"
+//                        url = "https://github.com/ConcerroX"
+//                    }
+//                }
+//                issueManagement {
+//                    system = "Github"
+//                    url = "https://github.com/ConcerroX/ripple-components-android/issues"
+//                }
+//            }
         }
     }
-}
-
-signing {
-    sign(publishing.publications.getByName("main"))
 }
 
 dependencies {
